ad2a8c316eb4952cc205434709f904e04f19a55f
igorgarciateran <igorgt@gmail.com>
2015-02-26 00:54:57 +0100

    en casa

---
+++ JavaScript/ej11/assets/js/code.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+		var valida=function(){
+			var elformu=document.getElementByid('registro');
+			
+			};
+    
+};
+
+
+
+
+// en el otro js => window.... function ... get element registro.validate
--- JavaScript/ej11/assets/js/validador.js
+++ JavaScript/ej11/assets/js/validador.js
@@ -3,26 +3,42 @@ HTMLFormElement.prototype.validate = function() {
 
     var validador = {
         required: function(val) {
+            //mirar que no este vacio
             return false;
         },
 
         email: function(val) {
-            return false;
+            //mirar que es un mail valido
+
+        //var Validador="/^.@([0-9.\-A-Za-z]+)$/";
+
+        return (val.test("/^.@([0-9.\-A-Za-z]+)$/"));
+
         }
     };
 
 
 
     var required = this.querySelectorAll('.required');
+    var email=this.querySelectorAll('.email');
 
 
     var validate = function() {
+        for (var i = required.length - 1; i >= 0; i--) {
+            validador (required[i]);
+        };
+
+
+        for (var i = email.length - 1; i >= 0; i--) {
+           validador (email[i]);
+        };
 
     };
 
 
 
     this.addEventListener('submit', validate, false);
+    console.log("Añadido handler");
 
     //campos requeridos, class=required
     //campo email class=email

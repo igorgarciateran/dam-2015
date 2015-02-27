879a79ba9678dc02867641060747868fa7012fa4
igorgarciateran <igorgt@gmail.com>
2015-02-27 19:05:16 +0100

    ejercicios hoy

---
+++ angularjs/ej01/assets/js/main.js
@@ -0,0 +1,17 @@
+var todoApp = angular.module('todoApp', []);
+todoApp.controller('TodoController', ['$scope', function($scope) {
+		console.log($scope);
+		var todos=[];
+
+		todos.push({done:false, task:'Aprender Angular JS'});
+		todos.push({done:false, task:'Aprender Ionic'});
+		todos.push({done:true, task:'Adorar JS'});
+
+		$scope.todos=todos;
+
+		$scope.addTodo=function(){
+			todos.push({done:false,task:$scope.inputText});
+			$scope.inputText="";
+		};
+
+	}]);

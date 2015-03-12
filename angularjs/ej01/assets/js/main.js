31d9b385a97ab241ab66106a929b5c4798404730
igorgarciateran <igorgt@gmail.com>
2015-03-12 15:19:57 +0100

    hoy1

--- angularjs/ej01/assets/js/main.js
+++ angularjs/ej01/assets/js/main.js
@@ -1,17 +1,35 @@
 var todoApp = angular.module('todoApp', []);
 todoApp.controller('TodoController', ['$scope', function($scope) {
-		console.log($scope);
-		var todos=[];
+    console.log($scope);
+    var todos = [];
 
-		todos.push({done:false, task:'Aprender Angular JS'});
-		todos.push({done:false, task:'Aprender Ionic'});
-		todos.push({done:true, task:'Adorar JS'});
+    todos.push({
+        done: false,
+        task: 'Aprender Angular JS'
+    });
+    todos.push({
+        done: false,
+        task: 'Aprender Ionic'
+    });
+    todos.push({
+        done: true,
+        task: 'Adorar JS'
+    });
 
-		$scope.todos=todos;
+    $scope.todos = todos;
 
-		$scope.addTodo=function(){
-			todos.push({done:false,task:$scope.inputText});
-			$scope.inputText="";
-		};
+    $scope.addTodo = function() {
+        todos.push({
+            done: false,
+            task: $scope.inputText
+        });
+        $scope.inputText = "";
+    };
 
-	}]);
+
+
+    $scope.deleteTodo = function() {
+        todo.splice(2, 1);
+    };
+
+}]);

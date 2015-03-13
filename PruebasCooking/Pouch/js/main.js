7d6f8d08452f623c61dd956c5cb2baee71dd1456
igorgarciateran <igorgt@gmail.com>
2015-03-13 19:12:20 +0100

    hoy

--- PruebasCooking/Pouch/js/main.js
+++ PruebasCooking/Pouch/js/main.js
@@ -1,24 +1,40 @@
 (function() {
 
     var db;
-    var Nombre = 'pruebax10';
+    var Nombre = 'pruebax11';
 
-    var $BotInsertar = $('insertar');
-    var $BotBorrar = $('borrar');
-    var $texto = $('texto');
+    var textId = document.getElementById("id");
+    var BotInsertar = document.getElementById("insertar");
+    var BotBorrar = document.getElementById("borrar");
+    var BotLeer = document.getElementById("leer");
+    var texto = document.getElementById("texto");
 
-
-    $BotInsertar.on('click', insertar($texto));
-    console.log('añadido evento');
-
-
-
-
-    var Datos = {
-        //_id: '1',
-        nombre: 'primero'
-       // fecha: 1 / 10 / 15
+    var Receta = {
+        idReceta: 3,
+        nombreReceta: 'pepinillos en almibar3',
+        foto: ''
     };
+
+    var ddoc = {
+        _id: '_design/index',
+        views: {
+            index: {
+                map: function mapFun(doc) {
+                    if (doc.idReceta) {
+                        emit(doc.idReceta, doc.nombreReceta);
+                    }
+                }.toString()
+            },
+            otraDiferente: {
+                map: function mapfun2(doc) {
+                    if (doc.nombreReceta) {
+                        emit(doc.idreceta, doc.nombreReceta);
+                    }
+                }
+            }
+        }
+    };
+
 
 
 
@@ -31,11 +47,11 @@
 
 
     var insertar = function(Dato) {
-
-        console.log(Dato);
+        //console.log(Dato);
         db.post(Dato, function(err, result) {
             if (!err) {
                 console.log('Successfully posted :' + Dato.nombre);
+                console.log(Dato);
             } else {
                 console.log(err + '   ' + Dato.nombre);
             }
@@ -43,35 +59,22 @@
 
     };
 
-    var Modificar = function(Dato, Rev) {
-        db.put(DatoRev);
+    /*  var Modificar = function(Dato, Rev) {
+          db.put(DatoRev);
 
-        console.log('modificado');
-    };
-
-
-
-    var MeteDatos = function() {
-        insertar(Datos);
-        Datos._id = '2';
-        Datos.nombre = 'segundo';
-        insertar(Datos);
-        Datos._id = '3';
-        Datos.nombre = 'tercero';
-        insertar(Datos);
-        Datos._id = '4';
-        Datos.nombre = ' cuarta';
-        insertar(Datos);
-
-    };
+          console.log('modificado');
+      };   */
 
 
-    var LeeDatos = function(id) {
+    var LeeDatos = function() {
+        var id = textId.value;
         db.get(id.toString(), function(err, doc) {
             if (err) {
                 return console.log(err);
             } else {
                 console.log('leido : ' + doc);
+                console.log(doc);
+                //text.value=doc.nombreReceta;
             }
             // handle doc
         });
@@ -79,15 +82,94 @@
 
 
 
+    var Vista = function() {
+        // save the design doc
+        db.put(ddoc).catch(function(err) {
+            if (err.status !== 409) {
+                throw err;
+            }
+        });
+    };
 
 
+    var BusquedaVista = function() {
+        db.query('otraDiferente', {
+            //la key es el campo por el que quiero buscar
+            key: 'hola',
+            include_docs: true
+        }, function(err, response) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(response);
+        });
+    };
+
+
+
+    var Busqueda = function(doc) {
+        function map(doc) {
+            // join artist data to albums
+            if (doc.nombre) {
+                emit(doc.nombre, {
+                    id: doc.nombre,
+                    nombre: doc.fecha
+
+                });
+            }
+        }
+        db.query(map, {
+            include_docs: true
+        }, function(err, response) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(response);
+        });
+    };
+
+
+    var Meter = function() {
+        Receta.idReceta = textId.value;
+        Receta.nombreReceta = texto.value;
+        insertar(Receta);
+    };
+
+
+    var MeteDatos = function() {
+        /*insertar(Datos);
+        /*Datos._id = '12';
+         Datos.nombre = 'segundo';
+         Datos.fecha=new Date().toISOString();
+         insertar(Datos); 
+         Datos._id = '13';
+         Datos.nombre = 'tercero';
+         Datos.fecha=new Date().toISOString();
+         insertar(Datos);
+         Datos._id = '14';
+         Datos.nombre = ' cuarta';
+         Datos.fecha=new Date().toISOString();
+         insertar(Datos);/* */
+
+    };
 
 
     init();
+
+
+    BotInsertar.addEventListener('click', Meter, 'false');
+    BotLeer.addEventListener('click', LeeDatos, 'false');
+
+
+    //insertar(Receta);
+    //Vista();
     //MeteDatos();
     // LeeDatos(1);
+    //LeeDatos(4);
+    //LeeDatos(2);
+    //LeeDatos(3);
 
-
-
+    //Busqueda(Datos);
+    BusquedaVista();
 
 }());
